26c5d168ce7007dbaeb4d2be2729381e7fbca869
George Fox <georgefox1996@gmail.com>
2022-01-29 16:32:16 +0000

    Added tooltip to player list

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import VueApexCharts from 'vue-apexcharts'
 import CountryFlag from 'vue-country-flag'
+import VueCustomTooltip from '@adamdehaven/vue-custom-tooltip'
 
 Vue.config.productionTip = false
 
 Vue.use(VueApexCharts)
+Vue.use(VueCustomTooltip)
 
 Vue.component('apexchart', VueApexCharts)
 Vue.component('country-flag', CountryFlag)
