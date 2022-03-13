dab6340989157ba17cf609b60dc15dce542cad82
George Fox <georgefox1996@gmail.com>
2022-03-13 23:19:27 +0000

    Fixed node server to allow refresh of page

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,39 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-const history = require('connect-history-api-fallback');
+const express = require('express')
+const bodyParser = require('body-parser')
+const history = require('connect-history-api-fallback')
 
-const app = express();
-const path = require('path');
+const app = express()
+const path = require('path')
 const port = process.env.PORT || 3000
 
 // parse requests of content-type: application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // parse requests of content-type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(history());
+app.use(history())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    next();
-});
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+    next()
+})
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+app.use(
+    history({
+        index: '/index.html',
+    })
+)
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+})
 
-app.listen(port, () => console.log(`server started on port ${port}`));
+app.listen(port, () => console.log(`server started on port ${port}`))
