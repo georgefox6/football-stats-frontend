00186e5e9a0d25c65245cba39ef0b604d38d598e
George Fox <georgefox1996@gmail.com>
2022-03-15 18:27:52 +0000

    Attempted to fix history mode

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,20 @@ const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
 const history = require('connect-history-api-fallback')
-const cors = require('cors')
 
 const port = process.env.PORT || 3000
+
+app.use(history())
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json())
 
+app.use(history())
+
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.use(history())
-
-app.use(cors)
+app.use(history())
 
 
 app.use(function (req, res, next) {
@@ -28,6 +29,8 @@ app.use(function (req, res, next) {
     next()
 })
 
+app.use(history())
+
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 app.use(
@@ -36,8 +39,10 @@ app.use(
     })
 )
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
 })
+
+app.use(history())
 
 app.listen(port, () => console.log(`server started on port ${port}`))
