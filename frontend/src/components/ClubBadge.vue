cc3370a0b4ee80a3b376130b5528f80249481411
George Fox <georgefox1996@gmail.com>
2022-06-02 21:32:52 +0100

    Added bundesliga club badges and fixed player filtering for bundesliga clubs

--- frontend/src/components/ClubBadge.vue
+++ frontend/src/components/ClubBadge.vue
@@ -20,6 +20,24 @@
         <img v-if="clubName == 'Watford'" src="https://resources.premierleague.com/premierleague/badges/70/t57.png" alt="Watford">
         <img v-if="clubName == 'West Ham United'" src="https://resources.premierleague.com/premierleague/badges/70/t21.png" alt="West Ham United">
         <img v-if="clubName == 'Wolverhampton Wanderers'" src="https://resources.premierleague.com/premierleague/badges/70/t39.png" alt="Wolverhampton Wanderers">
+        <img v-if="clubName == 'Bayern Munich'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000G.svg" alt="Bayern Munich">
+        <img v-if="clubName == 'RB Leipzig'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000017.svg" alt="RB Leipzig">
+        <img v-if="clubName == 'Dortmund'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000007.svg" alt="Dortmund">
+        <img v-if="clubName == 'Wolfsburg'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000003.svg" alt="Wolfsburg">
+        <img v-if="clubName == 'Eintracht Frankfurt'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000F.svg" alt="Eintracht Frankfurt">
+        <img v-if="clubName == 'Bayer Leverkusen'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000B.svg" alt="Bayer Leverkusen">
+        <img v-if="clubName == 'Union Berlin'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000V.svg" alt="Union Berlin">
+        <img v-if="clubName == 'Mönchengladbach'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000004.svg" alt="Mönchengladbach">
+        <img v-if="clubName == 'Stuttgart'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000D.svg" alt="Stuttgart">
+        <img v-if="clubName == 'Freiburg'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000A.svg" alt="Freiburg">
+        <img v-if="clubName == 'Hoffenheim'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000002.svg" alt="Hoffenheim">
+        <img v-if="clubName == 'Mainz 05'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000006.svg" alt="Mainz 05">
+        <img v-if="clubName == 'Augsburg'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000010.svg" alt="Augsburg">
+        <img v-if="clubName == 'Hertha BSC'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000Z.svg" alt="Hertha BSC">
+        <img v-if="clubName == 'Arminia'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000015.svg" alt="Arminia">
+        <img v-if="clubName == 'Köln'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-000008.svg" alt="Köln">
+        <img v-if="clubName == 'Bochum'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000S.svg" alt="Bochum">
+        <img v-if="clubName == 'Greuther Fürth'" src="https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000W.svg" alt="Greuther Fürth">
     </div>
 </template>
 
